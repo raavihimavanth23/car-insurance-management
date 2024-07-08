cfb70d492a9c08818a125fe1a696e9f3af654ef2
raavihimavanth23 <52528996+raavihimavanth23@users.noreply.github.com>
2024-07-08 20:33:12 +0530

    Update settings.py

--- carinsurancemanagement/settings.py
+++ carinsurancemanagement/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'carinsurancemanagement.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME':  os.getenv('DB_NAME'),
+        'USER':  os.getenv('DB_USER'),
+        'PASSWORD':  os.getenv('DB_PASSWORD'),
+        'HOST':  os.getenv('DB_HOST'),
+        'PORT':  os.getenv('DB_PORT', default='3306'),
     }
 }
 
